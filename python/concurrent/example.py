1d87aa6b1bf1f8c5df309413e8cca4c8f857c242
coil398 <konayuta.sou@gmail.com>
2020-05-21 17:08:29 +0900

    add python

---
+++ python/concurrent/example.py
@@ -0,0 +1,33 @@
+import asyncio
+import time
+import requests
+import functools
+from concurrent.futures import ThreadPoolExecutor
+
+async def aSleep(seconds, value):
+    await asyncio.sleep(seconds)
+    return value * 10
+
+def synchronousFunc(Object: dict, number):
+    print('number: ', number)
+    return number
+
+async def aSleep2(loop, number):
+    func = functools.partial(synchronousFunc, Object=dict(), number=number)
+    res = await loop.run_in_executor(None, func)
+    return res
+
+loop = asyncio.get_event_loop()
+print(loop.run_until_complete(asyncio.gather(aSleep(1, 10), aSleep(3, 30), aSleep(2, 20))))
+loop = asyncio.get_event_loop()
+print(loop.run_until_complete(asyncio.gather(aSleep2(loop, 1), aSleep2(loop, 2), aSleep2(loop, 3), aSleep2(loop, 4), aSleep2(loop, 5))))
+
+def synchronousFunc2(num):
+    print(num)
+    return num
+
+with ThreadPoolExecutor(2) as e:
+    ret = e.map(synchronousFunc2, [number for number in range(1, 6)])
+
+for res in ret:
+    print('res: ', res)
